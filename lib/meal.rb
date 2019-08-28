8e081d7d20d928a4084bc0d4ade7ac535b886108
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 11:56:50 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -3,7 +3,7 @@ attr_accessor :waiter, :customer, :total, :tip
 
 @@all =[]
 
-def initialize(waiter, customer, total, tip)
+def initialize(waiter, customer, total, tip=0)
   @waiter = waiter
   @customer = customer
   @total = total
